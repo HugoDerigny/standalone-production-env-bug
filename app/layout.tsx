49d3ffee3325d4c875dad7f1756fb831f8c4f135
HugoDerigny <hugo.derigny@free.fr>
2023-11-10 12:30:06 +0100

    added files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({children}) {
+    console.log(`Environmnent is ${process.env.NODE_ENV}`);
+    console.log(process.env.MY_VALUE);
+    console.log(process.env.NEXT_PUBLIC_MY_VALUE);
+
+    return (
+        <html>
+        <head/>
+        <body>{children}</body>
+        </html>
+    )
 }
